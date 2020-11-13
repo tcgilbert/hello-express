cbb268a7cd745b4c8527fb287911818a6cb46e46
Thomas Gilbert <tcgilbert94@gmail.com>
2020-11-13 12:42:01 -0500

    blog and about routes

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ app.get('/', function(req, res) {
     res.send('hello, from express');
 });
 
+app.get('/about', function(req, res) {
+    res.send('this is the about page');
+});
+
+app.get('/blog', (req, res) => {
+    res.send('Welcome to my blog');
+});
+
 app.listen(8000);
